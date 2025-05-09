d8b6ecde67f0764c73b528e8f3f30adf3a68b3fa
Samrogrammer <samk5177@gmail.com>
2025-05-09 19:46:00 +0530

    updated start.sh and nixpacks

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@
 # Start FastAPI backnd
 uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000} &
 
-# Start Streamlit frontend
-streamlit run streamlit_app.py --server.port 8501
+streamlit run streamlit_app.py --server.port ${STREAMLIT_PORT:-8501} --server.address=0.0.0.0
